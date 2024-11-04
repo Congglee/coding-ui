cf89cc56e360f226eaf8f89b23faf3832173ea8a
Congglee <congldqn888@gmail.com>
2024-11-04 10:06:51 +0700

    Intro section

--- lasles-vpn/src/app/page.js
+++ lasles-vpn/src/app/page.js
@@ -1,9 +1,11 @@
 import HeroSection from "@/components/hero-section/hero-section";
+import IntroSection from "@/components/intro-section/intro-section";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
+      <IntroSection />
     </>
   );
 }
---
+++ lasles-vpn/src/components/intro-section/intro-section.js
@@ -0,0 +1,39 @@
+import styles from "./intro-section.module.css";
+
+export default function IntroSection() {
+  return (
+    <section className={styles.intro}>
+      <div className="container">
+        <div className={styles["intro-container"]}>
+          <div className={styles["intro-item"]}>
+            <div className={styles["intro-icon"]}>
+              <img srcSet="./images/user.png 2x" alt />
+            </div>
+            <div className={styles["intro-info"]}>
+              <h3 className={styles["intro-title"]}>90+</h3>
+              <span className={styles["intro-text"]}>Users</span>
+            </div>
+          </div>
+          <div className={styles["intro-item"]}>
+            <div className={styles["intro-icon"]}>
+              <img srcSet="./images/location.png 2x" alt />
+            </div>
+            <div className={styles["intro-info"]}>
+              <h3 className={styles["intro-title"]}>30+</h3>
+              <span className={styles["intro-text"]}>Locations</span>
+            </div>
+          </div>
+          <div className={styles["intro-item"]}>
+            <div className={styles["intro-icon"]}>
+              <img srcSet="./images/server.png 2x" alt />
+            </div>
+            <div className={styles["intro-info"]}>
+              <h3 className={styles["intro-title"]}>50+</h3>
+              <span className={styles["intro-text"]}>Servers</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
