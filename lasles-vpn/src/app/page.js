fbcbb6642275276af8406667c451016ca097f51f
Congglee <congldqn888@gmail.com>
2024-11-04 10:59:46 +0700

    Partner section

--- lasles-vpn/src/app/page.js
+++ lasles-vpn/src/app/page.js
@@ -1,6 +1,7 @@
 import FeatureSection from "@/components/feature-section/feature-section";
 import HeroSection from "@/components/hero-section/hero-section";
 import IntroSection from "@/components/intro-section/intro-section";
+import PartnerSection from "@/components/partner-section/partner-section";
 import PlanSection from "@/components/plan-section/plan-section";
 
 export default function Home() {
@@ -10,6 +11,7 @@ export default function Home() {
       <IntroSection />
       <FeatureSection />
       <PlanSection />
+      <PartnerSection />
     </>
   );
 }
---
+++ lasles-vpn/src/components/partner-section/partner-section.js
@@ -0,0 +1,25 @@
+import styles from "./partner-section.module.css";
+
+const partners = [
+  "./images/logo-netflix.png",
+  "./images/logo-reddit.png",
+  "./images/logo-amazon.png",
+  "./images/logo-discord.png",
+  "./images/logo-spotify.png",
+];
+
+export default function PartnerSection() {
+  return (
+    <section className={styles.partner}>
+      <div className="container">
+        <div className={styles["partner-container"]}>
+          {partners.map((partner, index) => (
+            <div className="partner-item" key={index}>
+              <img srcSet={partner + " 2x"} alt />
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
